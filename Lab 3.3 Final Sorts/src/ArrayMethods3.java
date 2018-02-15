1eaa2e3dab56a318c496dc39bda9394aac29d4b1
BT_1N3_33 <BT_1N3_33@BT_1N3_33.bktech.local>
2018-02-15 09:16:03 -0500

    yayay

--- Lab 3.3 Final Sorts/src/ArrayMethods3.java
+++ Lab 3.3 Final Sorts/src/ArrayMethods3.java
@@ -2,5 +2,22 @@
 //Period 3 & 4
 //2/14/18
 public class ArrayMethods3 {
+	public static void main (String args[]) {
+		//test code
+	}
 
+	public static String[] mergeSort(String[] list) {
+		
+	}
+	public static int partition(int [] list1, int front, int back) {
+		
+	}
+	
+	public static void quickSort(int[] list1, int front, int back) {
+		
+		
+	}
+	
+
+	
 }
